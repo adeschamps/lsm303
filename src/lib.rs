2cabba1752c97fe6b800776f22ed7e8ac5961a8a
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2017-08-17 22:40:07 -0400

    Implement basic magnetometer read.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub struct LSM303<Dev: I2CDevice>
     where Error: From<Dev::Error>
 {
     accel_device: Dev,
+    mag_device: Dev,
 }
 
 impl<Dev> LSM303<Dev>
@@ -28,16 +29,25 @@ impl<Dev> LSM303<Dev>
           Error: From<Dev::Error>
 {
     /// Initialize the sensor.
-    pub fn new(mut accel_device: Dev) -> Result<LSM303<Dev>> {
+    pub fn new(mut accel_device: Dev, mut mag_device: Dev) -> Result<LSM303<Dev>> {
         accel_device.smbus_write_byte_data(registers::ACCEL_CTRL_REG1_A, 0x27)?;
 
-        let mut reg4a = {
+        // Initialize the accelerometer
+        let mut reg4_a = {
             let bits = accel_device.smbus_read_byte_data(registers::ACCEL_CTRL_REG4_A)?;
             registers::CtrlReg4A::from_bits_truncate(bits)
         };
-        reg4a.set(registers::HR, true);
-        accel_device.smbus_write_byte_data(registers::ACCEL_CTRL_REG4_A, reg4a.bits())?;
-        let lsm303 = LSM303 { accel_device };
+        reg4_a.set(registers::HR, true);
+        accel_device.smbus_write_byte_data(registers::ACCEL_CTRL_REG4_A, reg4_a.bits())?;
+
+        // Initialize the magnetometer
+        let reg_m: registers::MrRegM = registers::MODE_CONTINUOUS;
+        mag_device.smbus_write_byte_data(registers::MAG_MR_REG_M, reg_m.bits())?;
+
+        let lsm303 = LSM303 {
+            accel_device,
+            mag_device,
+        };
         Ok(lsm303)
     }
 
@@ -60,6 +70,25 @@ impl<Dev> LSM303<Dev>
         let x = cursor.read_i16::<LittleEndian>()? >> 4;
         let y = cursor.read_i16::<LittleEndian>()? >> 4;
         let z = cursor.read_i16::<LittleEndian>()? >> 4;
+
+        let out = (x, y, z);
+        Ok(out)
+    }
+
+    pub fn read_magnetometer(&mut self) -> Result<(i16, i16, i16)> {
+
+        use byteorder::{BigEndian, ReadBytesExt};
+        use std::io::Cursor;
+
+        let data = self.mag_device
+            .smbus_read_i2c_block_data(registers::MAG_OUT_X_H_M, 6)?;
+
+        let mut cursor = Cursor::new(&data);
+
+        // Yes indeed, the registers are ordered as X, Z, Y
+        let x = cursor.read_i16::<BigEndian>()?;
+        let z = cursor.read_i16::<BigEndian>()?;
+        let y = cursor.read_i16::<BigEndian>()?;
 
         let out = (x, y, z);
         Ok(out)
