134060050ebdec643c103f562de52472bb685157
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2017-08-17 23:17:45 -0400

    Update the example code.

--- examples/read.rs
+++ examples/read.rs
@@ -14,14 +14,7 @@ quick_main!(run);
 fn run() -> Result<()> {
     let device = "/dev/i2c-1";
 
-    let accel_device = i2cdev::linux::LinuxI2CDevice::new(device, lsm303::constants::ADDRESS_ACCEL)
-        .chain_err(|| "Failed to open I2C device")?;
-
-    let mag_device = i2cdev::linux::LinuxI2CDevice::new(device, lsm303::constants::ADDRESS_MAG)
-        .chain_err(|| "Failed to open I2C device")?;
-
-    let mut lsm303 =
-        lsm303::LSM303::new(accel_device, mag_device).chain_err(|| "Failed to open LSM303 sensor")?;
+    let mut lsm303 = lsm303::LSM303::new(device).chain_err(|| "Failed to open LSM303 sensor")?;
 
     loop {
         let (a_x, a_y, a_z) = lsm303.read_accel()
