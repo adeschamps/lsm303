73f3079623b670cba490a0813ca627b0895495f4
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2017-08-17 22:40:20 -0400

    Update example to read magnetometer.

--- examples/read.rs
+++ examples/read.rs
@@ -17,13 +17,24 @@ fn run() -> Result<()> {
     let accel_device = i2cdev::linux::LinuxI2CDevice::new(device, lsm303::constants::ADDRESS_ACCEL)
         .chain_err(|| "Failed to open I2C device")?;
 
+    let mag_device = i2cdev::linux::LinuxI2CDevice::new(device, lsm303::constants::ADDRESS_MAG)
+        .chain_err(|| "Failed to open I2C device")?;
+
     let mut lsm303 =
-        lsm303::LSM303::new(accel_device).chain_err(|| "Failed to open LSM303 sensor")?;
+        lsm303::LSM303::new(accel_device, mag_device).chain_err(|| "Failed to open LSM303 sensor")?;
 
     loop {
-        let (x, y, z) = lsm303.read_accel()
+        let (a_x, a_y, a_z) = lsm303.read_accel()
             .chain_err(|| "Failed to read the accelerometer")?;
-        println!("Accel: {}, {}, {}", x, y, z);
+        let (m_x, m_y, m_z) = lsm303.read_magnetometer()
+            .chain_err(|| "Failed to read the magnetometer")?;
+        println!("Accel: {}, {}, {}  ||  Mag: {}, {}, {}",
+                 a_x,
+                 a_y,
+                 a_z,
+                 m_x,
+                 m_y,
+                 m_z);
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
