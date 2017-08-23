bc44156debdaa1d2d1dc2079dc45b88b851a2028
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2017-08-23 12:02:58 -0400

    Add links to extra resources in the readme and root documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 
 //! Interface to the LSM303 digital accelerometer and magnetometer.
 //!
-//! [Datasheet](http://www.st.com/resource/en/datasheet/lsm303dlhc.pdf)
+//! - [Datasheet](http://www.st.com/resource/en/datasheet/lsm303dlhc.pdf)
+//! - [Application notes](http://www.st.com/content/ccc/resource/technical/document/application_note/e6/f0/fa/af/94/5e/43/de/CD00269797.pdf/files/CD00269797.pdf/jcr:content/translations/en.CD00269797.pdf)
 
 // External crates
 
