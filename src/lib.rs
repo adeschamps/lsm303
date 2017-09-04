4e8d9e830e27e9b8e931c0a7690c47f33abfdd58
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2017-09-03 21:17:27 -0400

    Add usage example to the top level crate documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,28 @@
 //!
 //! - [Datasheet](http://www.st.com/resource/en/datasheet/lsm303dlhc.pdf)
 //! - [Application notes](http://www.st.com/content/ccc/resource/technical/document/application_note/e6/f0/fa/af/94/5e/43/de/CD00269797.pdf/files/CD00269797.pdf/jcr:content/translations/en.CD00269797.pdf)
+//!
+//! ```no_run
+//! # use std::time::Duration;
+//! # fn main() { test().unwrap(); }
+//! # fn test() -> lsm303::Result<()> {
+//! let device = "/dev/i2c-1";
+//! let mut accelerometer =
+//!     lsm303::Accelerometer::new(device)?;
+//! let mut magnetometer =
+//!     lsm303::Magnetometer::new(device)?;
+//!
+//!  loop {
+//!     let accel = accelerometer.read_acceleration()?;
+//!     let mag = magnetometer.read_magnetic_field()?;
+//!     println!("Accel: ({}, {}, {})  ||  Mag: ({}, {}, {})",
+//!              accel.x, accel.y, accel.z,
+//!              mag.x, mag.y, mag.z);
+//!     std::thread::sleep(Duration::from_millis(100));
+//! }
+//! # Ok(())
+//! # }
+//! ```
 
 // External crates
 
